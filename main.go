3f5e9b877254e8198209aef46fe69834cb0ef88b
Victor Eronmosele <victore@activedgetechnologies.com>
2020-09-13 09:53:51 +0100

    Add vendor folder

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = ":3000"
+	}
+
+	r := mux.NewRouter()
+
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+
+	// Serve index page on all unhandled routes
+	r.Handle("/", Handler())
+	r.PathPrefix("/styles/").Handler(http.StripPrefix("/styles/",
+		http.FileServer(http.Dir("static/styles/"))))
+
+	log.Fatal(http.ListenAndServe(port, r))
+
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	http.ServeFile(w, r, "./static/404.html")
+
+}
+
+func Handler() http.Handler {
+	return http.FileServer(http.Dir("static/"))
+}
